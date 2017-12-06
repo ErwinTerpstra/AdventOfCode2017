e24a99364bf4990ef307ba07bee7e553ba12ac7b
Erwin Terpstra <erwinterpstra@gmail.com>
2017-12-06 08:50:02 +0100

    Day 6. Still too easy. Another brute force solution. Could've hashed old states for faster checking. The second part was very trivial.

---
+++ day6b.js
@@ -0,0 +1,74 @@
+var input = '5	1	10	0	1	7	13	14	3	12	8	10	7	12	0	6';
+var banks = input.trim().split('\t').map(function(blocks) { return parseInt(blocks); });
+
+var oldStates = [ ];
+
+var duplicate = false;
+var cycles = 0;
+
+var loopLength;
+
+do
+{
+	// Save old state
+	var state = banks.slice();
+	oldStates.push(state);
+
+	// Find bank with most blocks
+	var mostBlocks = -1;
+	var mostBlocksIdx = -1;
+
+	for (var bankIdx = 0; bankIdx < banks.length; ++bankIdx)
+	{
+		var blocks = banks[bankIdx];
+
+		if (blocks > mostBlocks)
+		{
+			mostBlocks = blocks;
+			mostBlocksIdx = bankIdx;
+		}
+	}
+
+	// Balance blocks
+	banks[mostBlocksIdx] = 0;
+
+	var blocksLeft = mostBlocks;
+	var bankIdx = mostBlocksIdx;
+
+	while (blocksLeft > 0)
+	{
+		bankIdx = (bankIdx + 1) % banks.length;
+		++banks[bankIdx];
+		--blocksLeft;
+	}
+
+	// Increase cycle count
+	++cycles;
+
+	// Check if this state has occurred before
+	for (var stateIdx = 0; stateIdx < oldStates.length; ++stateIdx)
+	{
+		var oldState = oldStates[stateIdx];
+
+		var different = false;
+
+		for (var bankIdx = 0; bankIdx < banks.length; ++bankIdx)
+		{
+			if (banks[bankIdx] != oldState[bankIdx])
+			{
+				different = true;
+				break;
+			}
+		}
+
+		if (!different)
+		{
+			duplicate = true;
+			loopLength = cycles - stateIdx;
+			break;
+		}
+	}
+} while (!duplicate);
+
+console.log(cycles);
+console.log(loopLength);
