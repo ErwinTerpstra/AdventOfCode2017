f1c6e370f09530f8545d03fc555f9f0b2f9e6886
Erwin Terpstra <erwinterpstra@gmail.com>
2017-12-10 12:14:18 +0100

    Day 9. Okay, something that prevents just using string.split. Not very hard.

---
+++ day9b.js
@@ -0,0 +1,81 @@
+var fs = require('fs');
+
+var input = fs.readFileSync('day9_input.txt', 'utf8').trim();
+
+var rootGroup = null;
+var currentGroup = null;
+var inGarbageGroup = false;
+var escaped = false;
+
+var totalScore = 0;
+var garbageCharacters = 0;
+
+for (var characterIdx = 0; characterIdx < input.length; ++characterIdx)
+{
+	var character = input.charAt(characterIdx);
+
+	if (inGarbageGroup)
+	{
+		if (!escaped)
+		{
+			switch (character)
+			{
+				case '!':
+					escaped = true;
+					break;
+
+				case '>':
+					inGarbageGroup = false;
+					break;
+
+				default:
+					++garbageCharacters;
+					break;
+			}
+		}
+		else
+			escaped = false;
+	}
+	else
+	{
+		switch (character)
+		{
+			case '{':
+				// Start of new group
+				currentGroup = 
+				{
+					parent: currentGroup,
+					children: [ ],	
+				};
+
+				if (currentGroup.parent != null)
+				{
+					currentGroup.parent.children.push(currentGroup);
+					currentGroup.score = currentGroup.parent.score + 1;
+				}
+				else
+				{
+					currentGroup.score = 1;
+					rootGroup = currentGroup;
+				}
+
+				totalScore += currentGroup.score;
+
+				break;
+
+			case '}':
+				currentGroup = currentGroup.parent;
+				break;
+
+			case '<':
+				inGarbageGroup = true;
+				break;
+
+			case ',':
+				break;
+		}
+	}
+}
+
+console.log(totalScore);
+console.log(garbageCharacters);
